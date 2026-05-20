8f31c06fe5b28103c13848e8c005cc4e51600b79
joaoviitordev <joaovitorgomesdefaria@gmail.com>
2026-05-20 16:54:00 -0300

    fix: implement sales and products management pages with data tables and creation forms

--- app/(components)/products/page.tsx
+++ app/(components)/products/page.tsx
@@ -13,7 +13,7 @@ const ProductsPage = async () => {
   }));
 
   return (
-    <div className="w-full space-y-8 p-8">
+    <div className="w-full space-y-4 p-4">
       <div className="w-full flex items-center justify-between">
         <div className="space-y-1">
           <span className="text-sm text-gray-500 font-semibold">
--- app/(components)/sales/page.tsx
+++ app/(components)/sales/page.tsx
@@ -14,7 +14,7 @@ export default async function SalesPage() {
   }));
 
   return (
-    <div className="w-full space-y-8 p-8">
+    <div className="w-full space-y-4 p-4">
       <div className="w-full flex items-center justify-between">
         <div className="space-y-1">
           <span className="text-sm text-gray-500 font-semibold">
